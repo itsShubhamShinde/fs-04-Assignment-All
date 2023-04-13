b18ece4a9b903ca8aca3ab2423660cb26cb840e7
itsShubhamShinde <itsshubhamks@gmail.com>
2023-04-13 16:29:25 +0530

    routing assignment

---
+++ Routing/routing/src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Styling/Navbar.css"
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='navbarDiv'>
+        <h1 style={{color:"red"}}>React</h1>
+        <ul>
+            <Link to="/">
+            <li>Home</li>
+            </Link>
+            <Link to="/tech">
+            <li>Technologies</li>
+            </Link>
+            <Link to="/product">
+            <li>Products</li>
+            </Link>
+            <Link to="/contact">
+            <li>Contacts Us</li>
+            </Link>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar;
